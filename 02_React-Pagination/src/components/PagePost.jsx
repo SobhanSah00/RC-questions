734c5f9719d681ab8927b02f19f23f0809f47509
sobhan9012 <sobhansahoo2000@gmail.com>
2024-07-12 01:32:54 +0530

    "Added axios dependency, updated App.css, created PagePost and Pagination components, and added styles to index.css , adding pagination functionlaty"

--- 02_React-Pagination/src/components/PagePost.jsx
+++ 02_React-Pagination/src/components/PagePost.jsx
@@ -1,13 +1,26 @@
 import Pagination from "./Pagination"
+import {useState , useEffect } from "react"
+import axios from "axios"
 
 function PagePost() {
+  const [data,setData] = useState([])
+  const [currentPage,setCurrentPage] = useState(1)
+  // console.log(data);
+  useEffect(() => {
+    axios.get(`https://picsum.photos/v2/list?page=${currentPage}&limit=5`)
+    .then((res) => {setData(res.data)})
+
+  },[currentPage])
   return (
     <div className="container">
-        PagePost
         <div className="post-container">
-            
+            {
+              data.map((item,index) => {
+                return <img key={index} src={item.download_url} alt="" />
+              })
+            }
         </div>
-        <Pagination />
+        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </div>
   )
 }
--- 02_React-Pagination/src/components/Pagination.jsx
+++ 02_React-Pagination/src/components/Pagination.jsx
@@ -1,8 +1,27 @@
+function Pagination({ currentPage, setCurrentPage }) {
+  const handlePagination = (direction) => {
+    if (direction === 'next') {
+      setCurrentPage(currentPage + 1);
+    } else if (direction === 'prev') {
+      if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+      }
+    }
+  };
 
-function Pagination() {
   return (
-    <div>Pagination</div>
-  )
+    <div className="paginationContainer">
+      <button className="page-btn" onClick={() => handlePagination('prev')}>
+        {"<"}
+      </button>
+      <button className="page-btn" onClick={() => handlePagination('next')}>
+        {currentPage}
+      </button>
+      <button className="page-btn" onClick={() => handlePagination('next')}>
+        {">"}
+      </button>
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
