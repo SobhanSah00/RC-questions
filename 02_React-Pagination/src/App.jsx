35c4bf3a7dc0f0ebba544766c70f0452f6a212e2
sobhan9012 <sobhansahoo2000@gmail.com>
2024-07-11 16:21:26 +0530

    adding components

---
+++ 02_React-Pagination/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import PagePost from './components/PagePost'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>React Pagination</h1>
+        <PagePost />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 02_React-Pagination/src/components/PagePost.jsx
@@ -0,0 +1,15 @@
+import Pagination from "./Pagination"
+
+function PagePost() {
+  return (
+    <div className="container">
+        PagePost
+        <div className="post-container">
+            
+        </div>
+        <Pagination />
+    </div>
+  )
+}
+
+export default PagePost
