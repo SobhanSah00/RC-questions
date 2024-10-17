08c7131c9499e06243e4eb7e907f656bf6beb9d1
sobhan9012 <sobhansahoo2000@gmail.com>
2024-10-17 10:32:13 +0530

    creating the quiz app with useReducer function

--- 17_quiz_App/src/App.jsx
+++ 17_quiz_App/src/App.jsx
@@ -1,7 +1,35 @@
-import {useState} from "react";
+import {useState,useReducer, useEffect} from "react";
 import "./App.css"
+import quizData from "./data/quizData.json"
+
+const initialState = {
+  questions : [],
+  status: 'loading',
+  index : 0,
+  answer : null,
+  secondsRemaining: null
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'dataRecived':
+      return {
+        ...state,
+        questions : action.questions,
+        status : 'ready'
+      }
+  }
+}
 
 function App() {
+
+  const [{questions, status, index, answer, secondsRemaining},dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({type : 'dataRecived', payload: quizData})
+  })
+
+
   return (
     <div>App</div>
   )
